7fe4910dc3313f4afff8698d532ad7ce519b35fe
‪Mohamed Foullane‬‏ <142094528+Foullane-Mohamed@users.noreply.github.com>
2025-12-12 11:50:35 +0100

    ajouter pages tires et trailers et trips et dashboard et fuels et maintenances et tires

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,12 @@ connectDB();
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 // Swagger API Documentation
 app.use(
