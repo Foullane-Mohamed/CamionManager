03a3bb4aefc9fad01df0da0b64a0556452011715
‪Mohamed Foullane‬‏ <142094528+Foullane-Mohamed@users.noreply.github.com>
2025-12-17 00:55:19 +0100

    fix sidebar to be dynamique

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           <AppRoutes />
           <ToastContainer
             position="top-right"
-            autoClose={3000}
+            autoClose={2000}
             hideProgressBar={false}
             newestOnTop
             closeOnClick
